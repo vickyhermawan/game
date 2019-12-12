07b4e6423b13ea0f6f62686269c0abbee816744e
ilhamwibisono13 <ilham.wibisono@mail.ugm.ac.id>
2019-12-12 23:17:17 +0700

    UIPause dan Enenemy

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,38 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 { 
+    public static Pause Instance;
     public GameObject pauseMenuUI;
     public bool isPaused;
     
-    private void Update()
+    private void Start()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            isPaused = !isPaused;
-        }
-
-        if(isPaused){
-            ActivateMenu();
-            Time.timeScale = 0;
-        }
-        else
-        {
-            DeactivateMenu();
-            Time.timeScale = 1;
-        }
-
+        Instance = this;
+        pauseMenuUI.SetActive(false);
     }
 
-    void ActivateMenu()
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)||Input.GetKeyDown(KeyCode.Space))
+        {
+            isPaused = !isPaused;
+
+            if(isPaused){
+                ActivateMenu();
+            }
+            else
+            {
+                DeactivateMenu();
+            }
+        }
+    }
+
+    public void ActivateMenu()
     {
         pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        isPaused = true;
     }
 
     public void DeactivateMenu()
     {
         pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+        Debug.Log("Game is Exit");
+    }
+
+    public void ToMainMenu(){
+        SceneManager.LoadScene("MenuScene");
+        isPaused = false;
+        Time.timeScale = 1;
     }
 }
