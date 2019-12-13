3a2eef079a392421bba8670ba97d5b092b73ce6d
ilhamwibisono13 <ilham.wibisono@mail.ugm.ac.id>
2019-12-13 10:28:17 +0700

    tes unrevert

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -1,58 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 { 
-    public static Pause Instance;
     public GameObject pauseMenuUI;
     public bool isPaused;
     
-    private void Start()
+    private void Update()
     {
-        Instance = this;
-        pauseMenuUI.SetActive(false);
-    }
-
-    public void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape)||Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
             isPaused = !isPaused;
-
-            if(isPaused){
-                ActivateMenu();
-            }
-            else
-            {
-                DeactivateMenu();
-            }
         }
+
+        if(isPaused){
+            ActivateMenu();
+            Time.timeScale = 0;
+        }
+        else
+        {
+            DeactivateMenu();
+            Time.timeScale = 1;
+        }
+
     }
 
-    public void ActivateMenu()
+    void ActivateMenu()
     {
         pauseMenuUI.SetActive(true);
-        Time.timeScale = 0;
-        isPaused = true;
     }
 
     public void DeactivateMenu()
     {
         pauseMenuUI.SetActive(false);
-        Time.timeScale = 1;
-        isPaused = false;
-    }
-
-    public void QuitGame(){
-        Application.Quit();
-        Debug.Log("Game is Exit");
-    }
-
-    public void ToMainMenu(){
-        SceneManager.LoadScene("MenuScene");
-        isPaused = false;
-        Time.timeScale = 1;
     }
 }
