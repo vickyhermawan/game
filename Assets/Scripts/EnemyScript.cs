7318cf95bda27671524f2b8ab49c98acce329a87
ilhamwibisono13 <ilham.wibisono@mail.ugm.ac.id>
2019-12-13 10:21:54 +0700

    Revert "Merge pull request #13 from vickyhermawan/wibi"
    This reverts commit 84ec014e51430ec7cd534f3409ac50ddfdfbb351, reversing
    changes made to e06da54d704245b91fd3ae19a485cb2e57f0712b.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    public int health;
     public GameObject destructionVFX;
 
     public GameObject EnemyShoot;
 
-    public int shotChance; //probability of 'Enemy's' shooting during tha path
-    public float shotTimeMin, shotTimeMax; //max and min time for shooting from the beginning of the path
+    [HideInInspector] public int shotChance; //probability of 'Enemy's' shooting during tha path
+    [HideInInspector] public float shotTimeMin, shotTimeMax; //max and min time for shooting from the beginning of the path
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("ActivateShooting", Random.Range(shotTimeMin,shotTimeMax));
+        Invoke("ActivateShooting", 2);
     }
 
     void ActivateShooting() 
     {
-        if (Random.value < (float)shotChance / 1)                             //if random value less than shot probability, making a shot
+        if (Random.value < (float)shotChance / 100)                             //if random value less than shot probability, making a shot
         {                         
             Instantiate(EnemyShoot, gameObject.transform.position, Quaternion.identity);             
         }
@@ -31,11 +30,7 @@ public class EnemyScript : MonoBehaviour
     }
 
      private void OnTriggerEnter2D(Collider2D other){
-        if(other.transform.tag == "Shoot"){
-            health--;
-            Instantiate(destructionVFX, transform.position, Quaternion.identity); 
-            if (health == 0)
-            {
+        if(other.transform.tag == "Shoot"){ 
             Instantiate(destructionVFX, transform.position, Quaternion.identity);       
             Destroy(this.gameObject);
             SoundEffectManager.Instance.Explotion();
